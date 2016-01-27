20103d756aabe0470693052417f0ffbc2549757a
Xin Du <xdu3@studentmail.nait.ca>
2016-01-27 14:00:47 -0700

    add a admin controller

---
+++ CrystalBallSolution/CrystalBallSystem/BLL/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+#region Additional namespace
+
+#endregion
+
+namespace CrystalBallSystem.BLL
+{
+    public class AdminController
+
+    {
+    }
+}
