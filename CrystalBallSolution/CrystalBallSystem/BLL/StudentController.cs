6d22602fbab75acccd02a671724c6ce55d770ef1
kgibson5 <kgibson5@WB304-03.nait.ca>
2016-02-03 14:54:04 -0700

    fixed student controller

--- CrystalBallSolution/CrystalBallSystem/BLL/StudentController.cs
+++ CrystalBallSolution/CrystalBallSystem/BLL/StudentController.cs
@@ -14,46 +14,12 @@ using CrystalBallSystem.DAL.POCOs;
 
 #endregion
 
-/*
+
 namespace CrystalBallSystem.BLL
 {
     [DataObject]
     public class StudentController
     {
-        #region account setup
-        public void Registration(Student item)
-        {
-            using (CrystalBallContext context = new CrystalBallContext())
-            {
-                // TODO: Validation rules...
-                var added = context.Students.Add(item);
-                context.SaveChanges();
-            }
-        }
-
-        public void ChangePassword(Student item)
-        {
-            using (CrystalBallContext context = new CrystalBallContext())
-            {
-                // TODO: Validation...
-                var attached = context.Students.Attach(item);
-                var existing = context.Entry<Student>(attached);
-                existing.State = System.Data.Entity.EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
-        public string AccountRecovery(string email)
-        {
-            using (CrystalBallContext context = new CrystalBallContext())
-            {
-                string result = (from account in context.Students
-                                 where (account.Email == email)
-                                 select account.Password).FirstOrDefault();
-                return result;
-            }
-        }
-        #endregion
-
         #region add nait course
         public void AddCourse(NaitCourse item)
         {
@@ -62,38 +28,6 @@ namespace CrystalBallSystem.BLL
                 // TODO: Validation rules...
                 var added = context.NaitCourses.Add(item);
                 context.SaveChanges();
-            }
-        }
-        #endregion
-
-        #region add high school course
-        public void AddHighSchoolCourse(CompletedHighSchoolCourse item)
-        {
-            using (CrystalBallContext context = new CrystalBallContext())
-            {
-                // TODO: Validation rules...
-                var added = context.CompletedHighSchoolCourses.Add(item);
-                context.SaveChanges();
-            }
-        }
-        #endregion
-
-        #region list high school courses
-        [DataObjectMethod(DataObjectMethodType.Select, false)]
-        public List<StudentHighSchoolCourses> CompletedHighSchoolCourses(int studentID)
-        {
-            using (var context = new CrystalBallContext())
-            {
-                var results = from row in context.CompletedHighSchoolCourses
-                              orderby row.HighSchoolCourseID
-                              where row.StudentID == studentID
-                              select new StudentHighSchoolCourses
-                              {
-                                  HighSchoolCourse = row.HighSchoolCourse.HighSchoolCourseName,
-                                  Mark = row.Mark
-                              };
-
-                return results.ToList();
             }
         }
         #endregion
@@ -117,4 +51,3 @@ namespace CrystalBallSystem.BLL
     }
 }
 
-*/
