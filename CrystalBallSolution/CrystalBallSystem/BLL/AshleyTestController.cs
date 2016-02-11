8e66857f6d16e2af113c6fbaeb62b84d587622ab
Ashley Condie <acondie1@studentmail.nait.ca>
2016-02-11 16:14:00 -0700

    Add Course Checkbox Page

---
+++ CrystalBallSolution/CrystalBallSystem/BLL/AshleyTestController.cs
@@ -0,0 +1,109 @@
+﻿using CrystalBallSystem.DAL;
+using CrystalBallSystem.DAL.POCOs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalBallSystem.BLL
+{
+    [DataObject]
+    public class AshleyTestController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<GetHSCourses> GetEnglishCourseList()
+        {
+            using (var context = new CrystalBallContext())
+            {
+                var results = from course in context.HighSchoolCourses
+                              where course.HighSchoolCourseName.Contains("English")
+                              orderby course.HighSchoolCourseName
+                              select new GetHSCourses
+                              {
+                                  HighSchoolCourseID = course.HighSchoolCourseID,
+                                  HighSchoolCourseDescription = course.HighSchoolCourseName
+                              };
+                return results.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<GetHSCourses> GetMathCourseList()
+        {
+            using (var context = new CrystalBallContext())
+            {
+                var results = from course in context.HighSchoolCourses
+                              where course.HighSchoolCourseName.Contains("Math")
+                              orderby course.HighSchoolCourseName
+                              select new GetHSCourses
+                              {
+                                  HighSchoolCourseID = course.HighSchoolCourseID,
+                                  HighSchoolCourseDescription = course.HighSchoolCourseName
+                              };
+                return results.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<GetHSCourses> GetSocialCourseList()
+        {
+            using (var context = new CrystalBallContext())
+            {
+                var results = from course in context.HighSchoolCourses
+                              where course.HighSchoolCourseName.Contains("Social")
+                              orderby course.HighSchoolCourseName
+                              select new GetHSCourses
+                              {
+                                  HighSchoolCourseID = course.HighSchoolCourseID,
+                                  HighSchoolCourseDescription = course.HighSchoolCourseName
+                              };
+                return results.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<GetHSCourses> GetScienceCourseList()
+        {
+            using (var context = new CrystalBallContext())
+            {
+                var results = from course in context.HighSchoolCourses
+                              where course.HighSchoolCourseName.Contains("Science") || 
+                                    course.HighSchoolCourseName.Contains("Physics") ||
+                                    course.HighSchoolCourseName.Contains("Chemistry") ||
+                                    course.HighSchoolCourseName.Contains("Biology")
+                              orderby course.HighSchoolCourseName
+                              select new GetHSCourses
+                              {
+                                  HighSchoolCourseID = course.HighSchoolCourseID,
+                                  HighSchoolCourseDescription = course.HighSchoolCourseName
+                              };
+                return results.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<GetHSCourses> GetOtherCourseList()
+        {
+            using (var context = new CrystalBallContext())
+            {
+                var results = from course in context.HighSchoolCourses
+                              where !course.HighSchoolCourseName.Contains("English") &&
+                                    !course.HighSchoolCourseName.Contains("Social") &&
+                                    !course.HighSchoolCourseName.Contains("Math") &&
+                                    !course.HighSchoolCourseName.Contains("Science") &&
+                                    !course.HighSchoolCourseName.Contains("Physics") &&
+                                    !course.HighSchoolCourseName.Contains("Chemistry") &&
+                                    !course.HighSchoolCourseName.Contains("Biology")
+                              orderby course.HighSchoolCourseName
+                              select new GetHSCourses
+                              {
+                                  HighSchoolCourseID = course.HighSchoolCourseID,
+                                  HighSchoolCourseDescription = course.HighSchoolCourseName
+                              };
+                return results.ToList();
+            }
+        }
+    }
+}
---
+++ CrystalBallSolution/CrystalBallWebSite/AshleyWorkspace/CourseSelect.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AshleyWorkspace_CourseSelect : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void submitCourseButton_Click(object sender, EventArgs e)
+    {
+        //clear the old cache
+        List<string> keys = new List<string>();
+        //retrieve application cache enumerator
+        IDictionaryEnumerator enumerator = Cache.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            keys.Add(enumerator.Key.ToString());
+        }
+        for (int k = 0; k < keys.Count; k++)
+        {
+            Cache.Remove(keys[k]);
+        }
+        HttpCookie test = new HttpCookie("DemoCookie");
+
+        for (int count = 0; count < EnglishList.Items.Count; count++)
+        {
+            if(EnglishList.Items[count].Selected)
+            {
+                test.Values["Courses"]
+                Response.Cookies.Add(test);
+            }
+        }
+    }
+}
