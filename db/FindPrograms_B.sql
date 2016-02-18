65f145136165652ad45d0de751dd3c26c3f26d56
brabideau1 <brabideau1@studentmail.nait.ca>
2016-02-18 16:38:17 -0700

    cleaned up procedure code

--- db/FindPrograms_B.sql
+++ db/FindPrograms_B.sql
@@ -11,31 +11,22 @@ go
 
 
 
-
 create procedure FindProgramsB @CourseList CourseIDs readonly
 as
 begin
-	--variable declaration
-	declare @programID int, @subjectID int, @entranceID int, @p_bool bit, @e_bool bit;
 
-	--output table?
-	declare @myPrograms table (ProgramID int);
-
-	set @p_bool = 1;  --check if I need these later
-	set @e_bool = 1;
-
-	insert into @myPrograms (ProgramID) (
-		select P.ProgramID from Program P where not exists ( --select the programs (?)
-			select 'x' from SubjectRequirement SR where exists (
+		select P.ProgramID from Program P where not exists (
+		--select the programs 
+			select 'x' from SubjectRequirement SR where exists ( 
+		--for which there is not a SubjectRequirement
 				select 'x' from EntranceRequirement ER where ER.SubjectRequirementID = SR.SubjectRequirementID and ER.ProgramID = P.ProgramID) and 
-			
-
-
+		--where the SubjectRequirement DOES exist for that program
 			not exists (
 				select 'y' from EntranceRequirement ER where ER.SubjectRequirementID = SR.SubjectRequirementID and ER.HighSchoolCourseID in (
-					select HSCID from @CourseList))));
+					select HSCID from @CourseList)));
+		--but the student has no natching courses
 
-	select ProgramID from @myPrograms
+
 
 end
 return
