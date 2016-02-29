013867a8d56c2bd017d402795f6e2bf7680b2a9f
brabideau1 <brabideau1@studentmail.nait.ca>
2016-02-29 15:10:45 -0700

    add program courses entity

---
+++ CrystalBallSolution/CrystalBallSystem/DAL/Entities/ProgramCourse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalBallSystem.DAL.Entities
+{
+    [Table("ProgramCourses")]
+    public partial class ProgramCourse
+    {
+        [Key]
+        [Column(Order = 0)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+
+        [Key]
+        [Column(Order = 0)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ProgramID { get; set; }
+
+        [Required]
+        public int? Semester {get; set;}
+
+        public virtual Program Program { get; set; }
+        public virtual NaitCours NaitCours { get; set; }
+    }
+}
